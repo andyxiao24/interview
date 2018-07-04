04c20f8e32479c831eee454669baa1d89a0df2b4
andyxiao24 <xp373150@163.com>
2018-07-04 16:05:12 +0800

    chencong

--- src/FiveOutOfTen/CheckResult.java
+++ src/FiveOutOfTen/CheckResult.java
@@ -3,7 +3,8 @@ package FiveOutOfTen;
 //需要实现下面的class，这个函数被调用的最近10次，如果有6次输入的是false，
 // 请输出“ERROR!”
 public class CheckResult {
+
     public void checkResult(boolean result) {
-        System.out.println("Error!");
-  }
+        System.out.println("ERROR!");
+    }
 }
---
+++ src/FiveOutOfTen/hisotry/C_2018_07_04.java
@@ -0,0 +1,36 @@
+package FiveOutOfTen.hisotry;
+
+/**
+ * Created by pengxiao on 2018/7/4.
+ * chencong
+ */
+public class C_2018_07_04 {
+    /*
+    package FiveOutOfTen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//需要实现下面的class，这个函数被调用的最近10次，如果有6次输入的是false，
+// 请输出“ERROR!”
+public class CheckResult {
+
+    private  List<Boolean> resList = new ArrayList();
+    private  Integer count = 0;
+
+    public  void checkResult(boolean param){
+        if (resList.size() <10){
+            resList.add(param);
+        }else if (resList.size() == 10){
+            for (Boolean item : resList) {
+                if (!item){
+            }
+        }else{
+            resList.remove(0);
+        }
+
+    }
+}
+
+     */
+}
