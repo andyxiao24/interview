d096c71a9ac53118044f767c5b3d8d794dc3ce9b
andyxiao24 <xp373150@163.com>
2018-07-06 13:18:41 +0800

    count rabbit o(1))

--- src/Rabiit/RabbitMain.java
+++ src/Rabiit/RabbitMain.java
@@ -1,11 +1,18 @@
 package Rabiit;
 
+import com.google.common.collect.Queues;
+
+import java.util.Queue;
+
 /**
  * Created by pengxiao on 2018/7/3.
  */
 public class RabbitMain {
     public static void main(String [] args) {
-        System.out.println(count(10));
+        for(int i = 1; i < 25; i++) {
+            System.out.println(String.format("f(%d)=%d, %d,", i, count(i), count2(i)));
+        }
+
     }
 
     public static long count(int n) {
@@ -20,5 +27,18 @@ public class RabbitMain {
             tmp[0] = sum * 2;
         }
         return sum * 3;
+    }
+
+    public static long count2(int n) {
+        Queue<Long> longQueue = Queues.newLinkedBlockingQueue(5);
+        longQueue.add(10l);
+        long sum = 10;
+        for (int i = 1; i < n; ++i) {
+            long toDie = i >= 5 ? longQueue.poll() : 0;
+            long newBorn = (sum - toDie) * 2;
+            sum = (sum - toDie) * 3;
+            longQueue.add(newBorn);
+        }
+        return sum;
     }
 }
